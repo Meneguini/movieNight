42cc1b24f3c9d7ca0128d82bbcc0d599df17e0c8
meneguini <mariana.meneguini@gmail.com>
2020-12-29 15:53:05 -0500

    check register/login

--- movies/models.py
+++ movies/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    # username = models.CharField(max_length=200, unique=True)
-    pass
-    # def __str__(self):
-    #     return f"User: {self.username}."
+    username = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f"User: {self.username}."
 
 
 class Movie(models.Model):
